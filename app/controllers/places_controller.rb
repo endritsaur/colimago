621ae2d81535f262b40136e1102471ff6147e29b
Endrit <endritsaur@gmail.com>
2017-12-11 20:52:29 -0600

    Added funcionality to edit places and minor bug fixes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,13 +8,31 @@ class PlacesController < ApplicationController
     end
     
     def new
+        @place = Place.new
+    end
+
+    def edit
+        @place = Place.find(params[:id])
     end
 
     def create
         @place = Place.new(place_params)
 
-        @place.save
-        redirect_to @place
+        if @place.save
+            redirect_to @place
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @place = Place.find(params[:id])
+
+        if @place.update(place_params)
+            redirect_to @place
+        else
+            render 'edit'
+        end
     end
 
     private
